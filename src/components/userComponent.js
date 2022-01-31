65802d796cbd4eda850e409ffa8de225f5050deb
gurhan <gurhantekoglu@gmail.com>
2022-01-31 20:44:16 +0300

    OOP 2

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -13,9 +14,8 @@ let user2 = new User(2, "Gürhan", "Tekoğlu", "Giresun")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-
-console.log(userService.getById(2))
+// console.log(userService.list())
+// console.log(userService.getById(2))
 
 let customer = {id:1, firstName:"Gürhan"}
 
@@ -23,3 +23,15 @@ let customer = {id:1, firstName:"Gürhan"}
 customer.lastName = "Tekoğlu"
 
 console.log(customer.lastName)
+
+console.log("----------------------")
+userService.load()
+
+let customerToAdd = new Customer(1, "Seda", "Yılmaz", "Ankara", "cba")
+customerToAdd.type = "customer"
+
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,58 @@
+export const users = [
+
+    {
+        id: 1,
+        firstName: "Engin",
+        lastName: "Demiroğ",
+        city: "Ankara",
+        age: 36,
+        creaditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Gürhan",
+        lastName: "Tekoğlu",
+        city: "Giresun",
+        age: 21,
+        creaditCardNumber: "654321",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Cansu",
+        lastName: "Değirmen",
+        city: "İstanbul",
+        salary: 8000,
+        age: 20,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Mücahit",
+        lastName: "Kaya",
+        city: "Bolu",
+        age: 23,
+        salary: 17000,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Eren",
+        lastName: "Arı",
+        city: "Diyarbakır",
+        age: 20,
+        salary: 15000,
+        type: "employee"
+    },
+    {
+        id: 6,
+        firstName: "Beytullah",
+        lastName: "Zor",
+        city: "Ankara",
+        age: 36,
+        creaditCardNumber: "123456",
+        type: "customer"
+    },
+
+]
